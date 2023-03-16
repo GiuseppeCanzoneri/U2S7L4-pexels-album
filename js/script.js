f9115abd62204307ffacd2d81a3763435a811ca6
GiuseppeCanzoneri <gcanzoneri6@gmail.com>
2023-03-17 00:41:32 +0100

    miglioramento-esercizio

--- js/script.js
+++ js/script.js
@@ -1,95 +1,89 @@
-// const API_KEY = "vTDACgPp81yauJry9DptfHJ2lvptJ7pGnZQpNfjBbq87aba96boFu6mH";
+const BASE_URL = "http://api.pexels.com/v1/";
+const PER_PAGE = 27;
+const API_KEY = "vTDACgPp81yauJry9DptfHJ2lvptJ7pGnZQpNfjBbq87aba96boFu6mH";
 
-// const imageContainer = document.getElementById("image-container");
-// const secondaryImageContainer = document.getElementById("secondary-image-container");
+const PRIMARY_QUERY = "friends";
 
-// async function loadImages(query, container) {
-//   try {
-//     const response = await fetch(`https://api.pexels.com/v1/search?query=${query}`, {
-//       headers: {
-//         Authorization: API_KEY,
-//       },
-//     });
-//     const data = await response.json();
-//     const images = data.photos
-//       .map(
-//         photo => `
-//       <div class="col-md-4">
-//         <div class="card">
-//           <img src="${photo.src.medium}" class="card-img-top" alt="${photo.photographer}">
-//           <div class="card-body">
-//             <h5 class="card-title">${photo.photographer}</h5>
-//           </div>
-//         </div>
-//       </div>
-//     `
-//       )
-//       .join("");
-//     container.innerHTML = images;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+const SECONDARY_QUERY = "amore";
 
-// loadImages("dog", imageContainer);
-// loadImages("montains", secondaryImageContainer);
+window.onload = async () => {
+  let inputSearch = document.getElementById("search-input");
 
-// definiamo le costanti per le URL delle API
-const PRIMARY_API_URL = "https://api.pexels.com/v1/search?query=dog";
-const SECONDARY_API_URL = "https://api.pexels.com/v1/search?query=montains";
+  const loadPrimaryImg = document.getElementById("primaryLoad");
 
-// selezioniamo gli elementi del DOM che vogliamo manipolare
-const loadImagesBtn = document.querySelector("#load-images-btn");
-const loadSecondaryImagesBtn = document.querySelector("#load-secondary-images-btn");
-const imageContainer = document.querySelector("#image-container");
-const secondaryImageContainer = document.querySelector("#secondary-image-container");
+  const loadSecondaryImg = document.getElementById("secondaryLoad");
 
-// funzione per caricare le immagini dalla API e mostrarle nel contenitore appropriato
-function loadImages(apiUrl, container) {
-  // impostiamo le opzioni della richiesta HTTP
-  const options = {
-    method: "GET",
-    headers: {
-      Authorization: "vTDACgPp81yauJry9DptfHJ2lvptJ7pGnZQpNfjBbq87aba96boFu6mH", // sostituire con la tua API key
-    },
+  const btnSearch = document.getElementById("search-btn");
+
+  loadPrimaryImg.addEventListener("click", () => fetchData(PRIMARY_QUERY));
+
+  loadSecondaryImg.addEventListener("click", () => fetchData(SECONDARY_QUERY));
+
+  btnSearch.addEventListener("click", () => {
+    const SEARCH_QUERY = inputSearch.value.trim();
+    inputSearch.value = "";
+    if (SEARCH_QUERY) {
+      fetchData(SEARCH_QUERY);
+    } else {
+      alert("Please enter a search term");
+    }
+  });
+
+  const fetchData = async parametro => {
+    try {
+      const response = await fetch(`https://api.pexels.com/v1/search?query=${parametro}&per_page=${PER_PAGE}`, {
+        headers: {
+          Authorization: API_KEY,
+        },
+      });
+
+      const images = await response.json();
+      showImg(images);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  // facciamo la richiesta HTTP alla API
-  fetch(apiUrl, options)
-    .then(response => response.json())
-    .then(data => {
-      // iteriamo sui risultati della ricerca e creiamo un elemento HTML per ciascuna immagine
-      data.photos.forEach(photo => {
-        const card = document.createElement("div");
-        card.classList.add("col-md-4");
-        card.innerHTML = `
-          <div class="card mb-4 shadow-sm">
-            <img src="${photo.src.medium}" class="bd-placeholder-img card-img-top">
-            <div class="card-body">
-              <h5 class="card-title">${photo.photographer}</h5>
-              <p class="card-text">${photo.url}</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-muted">${photo.width}x${photo.height}</small>
-              </div>
+  function showImg(images) {
+    if (images) {
+      let row = document.querySelector(".album .container .row");
+      row.innerHTML = "";
+      console.log(images.photos);
+      console.log(images.photos.lenght);
+      for (let i = 0; i < images.photos.length; i++) {
+        const col = document.createElement("div");
+        col.className = "col-md-4";
+        col.innerHTML = `    
+      <div class="card mb-4 shadow-sm">
+        <img src="${images.photos[i].src.tiny}" alt="Picture">
+        <div class="card-body">
+          <h5 class="card-title">Lorem Ipsum</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit
+            longer.
+          </p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <button type="button" class="btn btn-sm btn-outline-secondary">
+                View
+              </button>
+              <button type="button" class="btn btn-sm btn-outline-secondary" id="btnHide">
+                Hide
+              </button>
             </div>
+            <small class="text-muted">${images.photos[i].id}</small>
           </div>
-        `;
-        container.appendChild(card);
-      });
-    })
-    .catch(error => console.error(error));
-}
+        </div>
+      </div>
+      `;
 
-// quando viene premuto il bottone "Load Images", carichiamo le immagini primarie
-loadImagesBtn.addEventListener("click", () => {
-  loadImages(PRIMARY_API_URL, imageContainer);
-});
-
-// quando viene premuto il bottone "Load Secondary Images", carichiamo le immagini secondarie
-loadSecondaryImagesBtn.addEventListener("click", () => {
-  loadImages(SECONDARY_API_URL, secondaryImageContainer);
-});
+        const hideCard = col.querySelector("#btnHide");
+        hideCard.onclick = () => {
+          col.remove();
+        };
+        row.appendChild(col);
+      }
+    }
+  }
+};
